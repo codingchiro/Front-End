c571273d1301e4a7cf6df4a85bad8ff194c34cfd
Marc de la Torre <mdelatorre1314@gmail.com>
2019-05-20 16:09:50 -0700

    displaying quote info cards

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,7 +6,7 @@ import SearchResults from './SearchResults';
 export default class Search extends Component {
     state={
         quotes: [],
-        search: "",
+        searchValue: "",
     }
     
     handleInput = e => 
@@ -14,15 +14,16 @@ export default class Search extends Component {
 
     submitSearch = e => {
       e.preventDefault();
-      return (
-      <SearchResults
-        searchInput={this.search} />
-        )
+      window.location.reload()
     };
-
+    
+    
     componentDidMount(){
+      const searchValue= this.state.searchValue
+
       axios
-      .post("https://simpson-says-backend.herokuapp.com/users/search"
+      .post("https://simpson-says-backend.herokuapp.com/users/search",
+      searchValue
       )
       .then(response => {
         this.setState(() => ({ quotes: response.data }));
@@ -36,14 +37,19 @@ export default class Search extends Component {
     return (
       <div>
         <input type="text"
-              name="search"
-              value={this.search}
+              name="searchValue"
+              value={this.searchValue}
               onChange={this.handleInput}
               className="searchInput"
               placeholder="Search"></input>
         <button onClick={this.submitSearch}
                 className="Searchbutton"
                 type="submit">Search Quotes</button>
+        <div> { this.state.quotes.map( quote => 
+          <SearchResults 
+            quote={quote} 
+            key={quote.quote_id}/> ) } 
+        </div>
                 
       </div>
     )
--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 
 
 export default class SearchResults extends Component {
+  
   render() {
     return (
       <div>
-        <ul>
-          {this.props.quotes};
-        </ul>
-    </div>
+       <h1>
+         "{this.props.quote.spoken_words}"
+       </h1>
+       <h3>
+         Episode: {this.props.quote.episode_title}
+       </h3>
+       <p>
+         Character: {this.props.quote.raw_character_text} <br></br>
+         Season: {this.props.quote.season} ep. {this.props.quote.number_in_season}
+         
+       </p>
+      
+      </div>
     )
   }
 
